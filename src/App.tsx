6a301448678417f631bfc62824c07daec5a2fb0b
해원 <91236189+boriyoondal@users.noreply.github.com>
2023-02-23 15:36:49 +0900

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <section className="flex flex-col">
+        <p className="text-3xl text-blue-500">abc</p>
+        <div className="flex flex-col">
+          <input className="border border-solid" type="text" />
+          <input className="border border-solid" type="password" />
+        </div>
+        <button className="">btn</button>
+      </section>
     </div>
   );
 }
